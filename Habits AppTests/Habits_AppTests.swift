69dcd318a15bd96328c1f03b35ddf2797bf1d734
SShrey1 <palishrey@gmail.com>
2024-11-21 01:11:24 +0530

    Initial Commit

---
+++ Habits AppTests/Habits_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Habits_AppTests.swift
+//  Habits AppTests
+//
+//  Created by user@59 on 30/08/1946 Saka.
+//
+
+import Testing
+@testable import Habits_App
+
+struct Habits_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
